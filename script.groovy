7fa749dfe542ac30b1ba27f63ad4a0ab0722eb3f
Sathya <sathya20075@gmail.com>
2021-08-11 10:55:27 +0530

    updated script.groovy with return statement

--- script.groovy
+++ script.groovy
@@ -1,21 +1,22 @@
 import jenkins.model.*
 jenkins = Jenkins.instance
 
-class pipeline{
-	def compileApp(){
-	   echo "Building the application"
-	}
 
-	def testApp(){
-	   echo "Testing the application"
-	}
-
-	def buildApp(){
-	   echo "Building the application"
-	}
-
-	def deployApp(){
-	   echo "Deploying the application"
-	}
-
+def compileApp(){
+   echo "Building the application"
 }
+
+def testApp(){
+   echo "Testing the application"
+}
+
+def buildApp(){
+   echo "Building the application"
+}
+
+def deployApp(){
+   echo "Deploying the application"
+}
+
+return this
+

840aa8f1b522b129480aff08c1d8ce98f9d8bd37
Sathya <sathya20075@gmail.com>
2021-08-11 10:28:13 +0530

    added class structure to groovy script

--- script.groovy
+++ script.groovy
@@ -1,19 +1,21 @@
 import jenkins.model.*
 jenkins = Jenkins.instance
 
-def compileApp(){
-   echo "Building the application"
-}
+class pipeline{
+	def compileApp(){
+	   echo "Building the application"
+	}
 
-def testApp(){
-   echo "Testing the application"
-}
+	def testApp(){
+	   echo "Testing the application"
+	}
 
-def buildApp(){
-   echo "Building the application"
-}
+	def buildApp(){
+	   echo "Building the application"
+	}
 
-def deployApp(){
-   echo "Deploying the application"
-}
+	def deployApp(){
+	   echo "Deploying the application"
+	}
 
+}
